339545115e639b2e41df0f83c866bbe914d89563
Gerard <gkhynes@gmail.com>
2017-12-11 22:34:08 +0000

    Style index page

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
+const expressSanitizer = require("express-sanitizer");
 
 // APP CONFIG
 mongoose.connect("mongodb://localhost/restful_blog_app");
@@ -10,6 +11,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.use(expressSanitizer());
 
 // MONGOOSE/MODEL CONFIG
 const blogSchema = new mongoose.Schema({
@@ -42,6 +44,7 @@ app.get("/blogs/new", (req, res) => {
 //CREATE ROUTE
 app.post("/blogs", (req, res) => {
   // create blog
+  req.body.blog.body = req.sanitize(req.body.blog.body);
   Blog.create(req.body.blog, (err, newBlog) => {
     if (err) {
       res.render("new");
@@ -76,6 +79,7 @@ app.get("/blogs/:id/edit", (req, res) => {
 
 //UPDATE ROUTE
 app.put("/blogs/:id", (req, res) => {
+  req.body.blog.body = req.sanitize(req.body.blog.body);
   Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, updatedBlog) => {
     if (err) {
       res.redirect("/blogs");
